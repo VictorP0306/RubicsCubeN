9a525606c5e5e1f96f390523d74b62103c04d6a8
Victor Panin <victorpanin0306@gmail.com>
2020-12-29 00:00:10 +0700

    commit swing 28.12.2020

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import javax.swing.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Main();
+            }
+        });
+    }
+
+    Main() {
+
+        // Create a new JFrame container.
+        JFrame jfrm = new JFrame("A Simple Swing Program");
+
+        // Give the frame an initial size.
+        jfrm.setSize(275, 100);
+
+        // Terminate the program when the user closes the application.
+        jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create a text-based label.
+        JLabel jlab = new JLabel(" Swing powers the modern Java GUI.");
+
+        // Add the label to the content pane.
+        jfrm.getContentPane().add(jlab);
+
+        // Display the frame.
+        jfrm.setVisible(true);
     }
 }
